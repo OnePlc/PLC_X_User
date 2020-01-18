6d1228287545c234296d1e2d2e8a4ec0c8b4393c
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2020-01-18 10:54:31 +0100

    add version

--- src/Module.php
+++ src/Module.php
@@ -29,10 +29,17 @@ use Laminas\Session\Container;
 class Module
 {
     /**
+     * Module Version
+     *
+     * @since 1.0.1
+     */
+    const VERSION = '1.0.1';
+
+    /**
      * Load module config file
      *
-     * @since 1.0.0
      * @return array
+     * @since 1.0.0
      */
     public function getConfig() : array
     {
