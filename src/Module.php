ed8315069d33167107a1e8d49358572e11f9f048
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2020-02-07 20:54:55 +0100

    remove google signin

--- src/Module.php
+++ src/Module.php
@@ -31,9 +31,9 @@ class Module
     /**
      * Module Version
      *
-     * @since 1.0.8
+     * @since 1.0.9
      */
-    const VERSION = '1.0.8';
+    const VERSION = '1.0.9';
 
     /**
      * Load module config file
