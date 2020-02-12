ad4b1c398b345ba68e91aeee70d3d4e174783517
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2020-02-12 04:22:24 +0100

    api test

--- test/Controller/UserControllerTest.php
+++ test/Controller/UserControllerTest.php
@@ -18,6 +18,7 @@ declare(strict_types=1);
 
 namespace OnePlaceTest\User\Controller;
 
+use mysql_xdevapi\Exception;
 use OnePlace\User\Controller\UserController;
 use Application\Controller\CoreController;
 use Laminas\Stdlib\ArrayUtils;
@@ -384,6 +385,119 @@ class UserControllerTest extends AbstractHttpControllerTestCase
         $this->assertResponseStatusCode(200);
 
         $data = json_decode($this->getResponse()->getBody(), true);
-        $this->assertArrayHasKey('data', $data);
+        $this->assertArrayHasKey('message', $data);
+    }
+
+    /**
+     * @covers \OnePlace\User\Controller\ApiController::listAction
+     */
+    public function testAPIListIsLoading()
+    {
+        /**
+         * Init Test Session to Fake Login
+         */
+        $oSm = $this->getApplicationServiceLocator();
+        $oDbAdapter = $oSm->get(AdapterInterface::class);
+        $oTestUser = new User($oDbAdapter);
+        $oTestUser->exchangeArray([
+            'username' => 'travis',
+            'email' => 'travis@1plc.ch',
+            'id' => 1,
+            'full_name' => 'Travis CI',
+        ]);
+        CoreController::$oSession = new Container('plcauth');
+        CoreController::$oSession->oUser = $oTestUser;
+
+        $this->dispatch('/user/api/list', 'GET');
+        $this->assertResponseStatusCode(200);
+
+        $data = json_decode($this->getResponse()->getBody(), true);
+        if(!is_array($data)) {
+            var_dump($data);
+            throw new Exception('invalid api response');
+        }
+
+        $this->assertArrayHasKey('results', $data);
+    }
+
+    /**
+     * @covers \OnePlace\User\Controller\ApiController::addAction
+     */
+    public function testAPIAddFormIsLoading()
+    {
+        /**
+         * Init Test Session to Fake Login
+         */
+        $oSm = $this->getApplicationServiceLocator();
+        $oDbAdapter = $oSm->get(AdapterInterface::class);
+        $oTestUser = new User($oDbAdapter);
+        $oTestUser->exchangeArray([
+            'username' => 'travis',
+            'email' => 'travis@1plc.ch',
+            'id' => 1,
+            'full_name' => 'Travis CI',
+        ]);
+        CoreController::$oSession = new Container('plcauth');
+        CoreController::$oSession->oUser = $oTestUser;
+
+        $this->dispatch('/user/api/add', 'GET');
+        $this->assertResponseStatusCode(200);
+    }
+
+    /**
+     * @covers \OnePlace\User\Controller\ApiController::manageAction
+     */
+    public function testAPIManageListIsLoading()
+    {
+        /**
+         * Init Test Session to Fake Login
+         */
+        $oSm = $this->getApplicationServiceLocator();
+        $oDbAdapter = $oSm->get(AdapterInterface::class);
+        $oTestUser = new User($oDbAdapter);
+        $oTestUser->exchangeArray([
+            'username' => 'travis',
+            'email' => 'travis@1plc.ch',
+            'id' => 1,
+            'full_name' => 'Travis CI',
+        ]);
+        CoreController::$oSession = new Container('plcauth');
+        CoreController::$oSession->oUser = $oTestUser;
+
+        $this->dispatch('/user/api/manage', 'GET');
+        $this->assertResponseStatusCode(200);
+        $this->assertQuery('table.plc-core-basic-table');
+    }
+
+    /**
+     * @covers \OnePlace\User\Controller\ApiController::getAction
+     */
+    public function testAPIGetIsLoading()
+    {
+        /**
+         * Init Test Session to Fake Login
+         */
+        $oSm = $this->getApplicationServiceLocator();
+        $oDbAdapter = $oSm->get(AdapterInterface::class);
+        $oTestUser = new User($oDbAdapter);
+        $oTestUser->exchangeArray([
+            'username' => 'travis',
+            'email' => 'travis@1plc.ch',
+            'id' => 1,
+            'full_name' => 'Travis CI',
+        ]);
+        CoreController::$oSession = new Container('plcauth');
+        CoreController::$oSession->oUser = $oTestUser;
+
+        $this->dispatch('/user/api/get/1', 'GET');
+        $this->assertResponseStatusCode(200);
+
+        $data = json_decode($this->getResponse()->getBody(), true);
+        if(!is_array($data)) {
+            var_dump($data);
+            throw new Exception('invalid api response');
+        }
+
+        $this->assertArrayHasKey('oItem', $data);
     }
 }
